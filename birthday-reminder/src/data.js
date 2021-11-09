2c0ef895534863674939896e1781c086e89e8dd9
Alexandru Visan <alexandruvisan1908@gmail.com>
2021-11-09 13:56:29 +0200

    completed project

--- birthday-reminder/src/data.js
+++ birthday-reminder/src/data.js
@@ -1,8 +1,9 @@
 export default [
   {
     id: 1,
-    name: "Bertie Yates",
-    age: 29,
+    name: "Alex Visan",
+    age: 28,
+    birthday: [8, 2],
     image:
       "https://res.cloudinary.com/diqqf3eq2/image/upload/v1595959131/person-2_ipcjws.jpg",
   },
@@ -10,6 +11,7 @@ export default [
     id: 2,
     name: "Hester Hogan",
     age: 32,
+    birthday: [8, 2],
     image:
       "https://res.cloudinary.com/diqqf3eq2/image/upload/v1595959131/person-3_rxtqvi.jpg",
   },
@@ -17,6 +19,7 @@ export default [
     id: 3,
     name: "Larry Little",
     age: 36,
+    birthday: [8, 2],
     image:
       "https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883423/person-4_t9nxjt.jpg",
   },
@@ -24,6 +27,7 @@ export default [
     id: 4,
     name: "Sean Walsh",
     age: 34,
+    birthday: [8, 2],
     image:
       "https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883417/person-3_ipa0mj.jpg",
   },
@@ -31,6 +35,7 @@ export default [
     id: 5,
     name: "Lola Gardner",
     age: 29,
+    birthday: [8, 2],
     image:
       "https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883334/person-1_rfzshl.jpg",
   },
