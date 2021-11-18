3920965a812c0f4196f37e2cc0565a4c40acc1a6
Alexandru Visan <alexandruvisan1908@gmail.com>
2021-11-18 19:02:00 +0200

    removed files

--- todolist/src/components/UI/Button.js
+++
@@ -1,9 +0,0 @@
-// import styled from "styled-components";
-
-// const Button = styled.button`
-
-// const Button = (props) => {
-//   return <button>{props.children}</button>;
-// };
-
-// export default Button;
